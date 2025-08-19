105d64e73710bc9f0f9be60b82237743ba6b6d30
wtburdon <billyburdon@outlook.com>
2025-08-19 15:48:01 -0700

    made request contract for post

--- urlShortener.Contracts/Contracts/PostURLRequest.cs
+++ urlShortener.Contracts/Contracts/PostURLRequest.cs
@@ -1,6 +1,9 @@
 ﻿namespace urlShortener.Contracts.Contracts;
 
+/// <summary>
+/// Represents a request to shorten a URL.
+/// </summary>
 public record PostURLRequest
 {
-
+    string OriginalUrl { get; init; } = string.Empty;
 }
