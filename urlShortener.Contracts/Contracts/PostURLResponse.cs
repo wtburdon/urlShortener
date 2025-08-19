d73628b27bb1910a7ad215fac74aa67775cf524b
wtburdon <billyburdon@outlook.com>
2025-08-19 15:46:56 -0700

    made post response

--- urlShortener.Contracts/Contracts/PostURLResponse.cs
+++ urlShortener.Contracts/Contracts/PostURLResponse.cs
@@ -1,6 +1,17 @@
 ﻿namespace urlShortener.Contracts.Contracts;
 
+/// <summary>
+/// Represents the response after posting a URL to be shortened.
+/// </summary>
 public record PostURLResponse
 {
+    /// <summary>
+    /// Shortened URL
+    /// </summary>
+    public string ShortenedUrl { get; init; } = string.Empty;
 
+    /// <summary>
+    /// Original URL to be shortened
+    /// </summary>
+    public string OriginalUrl { get; init; } = string.Empty;
 }
--- urlShortener.Contracts/obj/Debug/net8.0/urlShortener.Contracts.AssemblyInfo.cs
+++ urlShortener.Contracts/obj/Debug/net8.0/urlShortener.Contracts.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("urlShortener.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75be98c1c8373fdc0863923c0a26d577dd772208")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa6fe02693eaf89c65c930d81bba8e72e05c8ee4")]
 [assembly: System.Reflection.AssemblyProductAttribute("urlShortener.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("urlShortener.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
