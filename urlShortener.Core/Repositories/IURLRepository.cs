04787921e6ff5a9ac6299da642f0ad4362647c22
wtburdon <billyburdon@outlook.com>
2025-08-19 17:05:24 -0700

    made some interface functions

--- urlShortener.Core/Repositories/IURLRepository.cs
+++ urlShortener.Core/Repositories/IURLRepository.cs
@@ -5,7 +5,19 @@
 /// </summary>
 public interface IURLRepository
 {
+    /// <summary>
+    /// Fetches repository for the shortend version of a URL
+    /// </summary>
+    /// <param name="originalUrl"></param>
+    /// <returns></returns>
     public Task<string> GetShortenedURLAsync(string originalUrl);
 
+    /// <summary>
+    /// Fetches repository for the original url with shortend version as key.
+    /// </summary>
+    /// <param name="shortenedUrl"></param>
+    /// <returns></returns>
+    public Task<string> GetOriginalURLAsync(string shortenedUrl);
 
+    public Task UploadURLDocument(string url);
 }
