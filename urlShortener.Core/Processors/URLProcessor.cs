4bb167cc1a50eb86a8e16d801a4163056ab25e38
wtburdon <billyburdon@outlook.com>
2025-08-22 10:25:30 -0700

    setup processor file

---
+++ urlShortener.Core/Processors/URLProcessor.cs
@@ -0,0 +1,25 @@
+﻿using urlShortener.Core.Repositories;
+
+namespace urlShortener.Core.Processors;
+
+/// </inheritdoc>
+public class URLProcessor:IURLProcessor
+{
+    private readonly IURLRepository repository;
+    URLProcessor(IURLRepository urlRepository)
+    {
+        repository = urlRepository;
+    }
+
+    public async Task<string> GetOriginalURLAsync(string shortenedUrl)
+    {
+        URLDocument doc = await repository.GetOriginalURLAsync(shortenedUrl);
+
+
+    }
+
+    public async Task<PostURLResponse> PostURLAsync(PostURLRequest request)
+    {
+
+    }
+}
