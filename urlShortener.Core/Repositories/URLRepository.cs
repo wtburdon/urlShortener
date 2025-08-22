5918ff2c2c04879145089469b8e3a487b2a4a513
William Burdon <billyburdon@outlook.com>
2025-08-21 18:42:53 -0700

    Implemented repository class (#3)
    * added azure cosmos w nuget
    
    * fixed package reference
    
    * added newtonsoft.json
    
    * added container to constructor
    
    * implemented Upload function
    
    * Delete urlShortener.Contracts/bin directory
    
    * Delete urlShortener.Contracts/obj directory
    
    * Delete urlShortener.Core/obj directory
    
    * Delete urlShortener.Core/bin/Debug/net8.0 directory
    
    * Delete urlShortener/bin/Debug/net8.0 directory
    
    * Delete urlShortener/obj directory

--- urlShortener.Core/Repositories/URLRepository.cs
+++ urlShortener.Core/Repositories/URLRepository.cs
@@ -3,5 +3,28 @@
 /// <inheritdoc/>
 public class URLRepository: IURLRepository
 {
+    private readonly Container container;
+    public URLRepository(Container container1)
+    {
+        container = container1;
+    }
 
+    public async Task<URLDocument> GetShortenedURLAsync(string originalUrl)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<URLDocument> GetOriginalURLAsync(string shortenedUrl)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UploadURLDocumentAsync(string originalUrl, string shortenedUrl)
+    {
+        await container.UpsertItemAsync<URLDocument>(new URLDocument
+        {
+            originalUrl = originalUrl,
+            shortenedUrl = shortenedUrl
+        });
+    }
 }
