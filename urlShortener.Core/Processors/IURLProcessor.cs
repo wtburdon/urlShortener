2e7afbf41111c3e063e836ee36bb3e7460b02381
wtburdon <billyburdon@outlook.com>
2025-08-22 10:22:08 -0700

    added xml comments for interface

--- urlShortener.Core/Processors/IURLProcessor.cs
+++ urlShortener.Core/Processors/IURLProcessor.cs
@@ -3,16 +3,16 @@
 public interface IURLProcessor
 {
     /// <summary>
-    /// 
+    /// Retrieves the original URL from the shortened URL.
     /// </summary>
-    /// <param name="shortenedUrl"></param>
-    /// <returns></returns>
-    public Task<string> GetURLAsync(string shortenedUrl);
+    /// <param name="shortenedUrl">Shortened URL</param>
+    /// <returns>String of the original URL</returns>
+    public Task<string> GetOriginalURLAsync(string shortenedUrl);
 
     /// <summary>
-    /// 
+    /// Uploads a a URL to the service
     /// </summary>
-    /// <param name="request"></param>
-    /// <returns></returns>
+    /// <param name="request">Request containing original URL</param>
+    /// <returns>Response contract with shortened URL</returns>
     public Task<PostURLResponse> PostURLAsync(PostURLRequest request);
 }
