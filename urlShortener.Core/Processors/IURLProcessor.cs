b4cf529eacf7da43eee4d57b4b08ad7cb283390a
wtburdon <billyburdon@outlook.com>
2025-08-22 10:08:14 -0700

    added functions for interface

--- urlShortener.Core/Processors/IURLProcessor.cs
+++ urlShortener.Core/Processors/IURLProcessor.cs
@@ -1,6 +1,18 @@
-﻿namespace urlShortener.Core.Processors
+﻿namespace urlShortener.Core.Processors;
+
+public interface IURLProcessor
 {
-    public interface IURLProcessor
-    {
-    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="shortenedUrl"></param>
+    /// <returns></returns>
+    public Task<string> GetURLAsync(string shortenedUrl);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    public Task<PostURLResponse> PostURLAsync(PostURLRequest request);
 }
